60cd8a80a2566bce935ec3d4cd3233dd901c92ae
EliasTengqvist <elias.tengqvist@skola.taby.se>
2024-05-25 15:10:05 +0200

    förbättringar
    Co-authored-by: Elin <Elaane@users.noreply.github.com>

--- game.js
+++ game.js
@@ -4,8 +4,8 @@ const SCREENWIDTH = 1000;
 const SCREENHEIGHT = 950;
 let gameCanvas = document.getElementById("gameCanvas");
 let c = gameCanvas.getContext("2d"); // Drawing object
-gameCanvas.height = SCREENHEIGHT/2;
-gameCanvas.width = SCREENWIDTH/2;
+gameCanvas.height = SCREENHEIGHT / 2;
+gameCanvas.width = SCREENWIDTH / 2;
 // -------------------------------------
 // Player variables
 let playerX = 300;
@@ -21,75 +21,81 @@ let directions = {
   down: false,
 };
 
-
 class lowerObstacle {
-  constructor(x,y,height,width) {
-      this.color = "black"; 
-      this.width = width; 
-      this.height = height; 
-      this.x = x;
-      this.y = y;
-      this.dx = -5;
-      this.dy = 0;
-  }
-
-}
-
-class upperObstacle {
-  constructor(x,y,height,width) {
-    this.color = "black"; 
-    this.width = width; 
-    this.height = height; 
+  constructor(x, y, height, width) {
+    this.color = "black";
+    this.width = width;
+    this.height = height;
     this.x = x;
     this.y = y;
     this.dx = -5;
     this.dy = 0;
-}
+  }
 }
 
-O1 = new lowerObstacle(500,300,200,50);
-O4 = new upperObstacle(500,-200,300,50);
-O3 = new lowerObstacle(20,30,40,50);
+class upperObstacle {
+  constructor(x, y, height, width) {
+    this.color = "black";
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.dx = -5;
+    this.dy = 0;
+  }
+}
 
-lowerObstacles = [O1, O3]
-upperObstacles = [O4]
+O1 = new lowerObstacle(500, 300, 200, 50);
+O4 = new upperObstacle(500, -200, 300, 50);
+O3 = new lowerObstacle(20, 30, 40, 50);
+
+lowerObstacles = [O1, O3];
+upperObstacles = [O4];
+
+setInterval(function createObstacles() {
+  let rand = Math.random() * 100 + 300;
+  let lower = new lowerObstacle(500, rand, 200, 50);
+  let upper = new upperObstacle(500, rand - 400, 250, 50);
+
+  lowerObstacles.push(lower);
+  upperObstacles.push(upper);
+}, 2000);
 
 // -------------------------------------
 // ------------ Player movement ------------
-/*document.addEventListener("keydown", (e) => {
+document.addEventListener("keydown", (e) => {
   switch (e.key) {
     case " ":
       directions.up = true;
       break;
   }
 });
-*/
+
 document.addEventListener("keyup", (e) => {
   switch (e.key) {
     case " ":
       directions.up = false;
       break;
-
   }
 });
 
-function GameOver (){
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-ctx.font = "48px sans-serif";
-ctx.fillStyle = "red";
-ctx.textAlign = "center";
-ctx.fillText("Game Over", 500, 500);
+/*function GameOver() {
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+  ctx.font = "48px sans-serif";
+  ctx.fillStyle = "red";
+  ctx.textAlign = "center";
+  ctx.fillText("Game Over", 500, 500);
 }
-
-// Animation 
+*/
+// Animation
 function animate() {
   requestAnimationFrame(animate); // Run gameloop recursively
   c.clearRect(0, 0, gameCanvas.width, gameCanvas.height); // Clear screen
 
   c.drawImage(bild, playerX, playerY, playerWidth, playerHeight); // Draw player
 
-
+  console.log(upperObstacles);
   for (let i = 0; i < upperObstacles.length; i++) {
     obs = upperObstacles[i];
     obs.x += obs.dx;
@@ -97,7 +103,6 @@ function animate() {
     // if (obs.x < playerX + playerWidth) {
     //  break;
     // }
-    
   }
 
   for (let i = 0; i < lowerObstacles.length; i++) {
@@ -107,21 +112,23 @@ function animate() {
     // if (obs.x < playerX + playerWidth) {
     //  break;
     // }
-    
   }
 
   if (directions.up) {
-    playerY -= 7;
+    playerY -= 15;
+    dy = 0;
   }
 
- if (playerY+ playerHeight < SCREENHEIGHT/2 + 27) ;{
+  if (playerY + playerHeight < SCREENHEIGHT / 2 + 27);
+  {
     playerY += dy;
-    }
-  dy += 0.1
-
-  if (playerY + 23 >= 950){
-    GameOver()
   }
+  dy += 0.3;
+
+  /*if (playerY + 23 >= 950) {
+    GameOver();
+  }
+  */
 }
 
 bild = document.createElement("img");
